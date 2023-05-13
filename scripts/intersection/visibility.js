3f3e3940db08d2acc0df3cc0729f22bc20a81416
Niklas Andersson <3985238+niklaspandersson@users.noreply.github.com>
2023-05-13 20:02:48 +0200

    Refactor visibility control

--- scripts/intersection/visibility.js
+++ scripts/intersection/visibility.js
@@ -5,7 +5,7 @@
 export default function (entries, sections) {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.classList.add("visible")
+      entry.target.classList.add("visible");
     }
     else {
       entry.target.classList.remove("visible");
---
+++ scripts/visibility/index.js
@@ -0,0 +1,2 @@
+const animated = document.querySelectorAll(".animate");
+animated.forEach(el => el.classList.add("hidden"));
